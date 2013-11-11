43f4bd7b55de81f7a912771e9bc6392fd78a071f
Todor Pitekov <esembidi@gmail.com>
2013-11-12 00:08:29 +0200

    added parser class

---
+++ ChessHeroSrv/src/com/kt/Parser.java
@@ -0,0 +1,16 @@
+package com.kt;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Toshko
+ * Date: 11/12/13
+ * Time: 12:03 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Parser
+{
+    public Object messageFromData(byte data[])
+    {
+        return new Object();
+    }
+}
