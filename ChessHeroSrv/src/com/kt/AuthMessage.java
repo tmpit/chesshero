f109f2ebf314bd0717f44a35cedfa71825586ed3
Todor Pitekov <esembidi@gmail.com>
2013-11-16 14:31:05 +0200

    now verifying action in constructor and implemented toData

--- ChessHeroSrv/src/com/kt/AuthMessage.java
+++ ChessHeroSrv/src/com/kt/AuthMessage.java
@@ -1,5 +1,8 @@
 package com.kt;
 
+import java.nio.BufferOverflowException;
+import java.nio.ByteBuffer;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Toshko
@@ -12,8 +15,13 @@ public class AuthMessage extends Message
     private int action;
     private Credentials credentials;
 
-    AuthMessage(int action, Credentials credentials)
+    AuthMessage(int action, Credentials credentials) throws ChessHeroException
     {
+        if (!Message.isActionValid(action))
+        {
+            throw new ChessHeroException(ChessHeroException.INVALID_ACTION_ERROR);
+        }
+
         this.action = action;
         this.credentials = credentials;
     }
@@ -26,5 +34,24 @@ public class AuthMessage extends Message
     public Credentials getCredentials()
     {
         return credentials;
+    }
+
+    @Override
+    public byte[] toData()
+    {
+        byte nameData[] = credentials.getName().getBytes();
+        byte passData[] = credentials.getPass().getBytes();
+
+        int bodyLen = 2 + 2 + nameData.length + 2 + passData.length; // Action + name length + name + pass length + pass
+
+        ByteBuffer messageData = ByteBuffer.allocate(Message.HEADER_LENGTH + bodyLen);
+        messageData.putShort((short)bodyLen); // Put header
+        messageData.putShort((short)action); // Put action
+        messageData.putShort((short)nameData.length); // Put name length
+        messageData.put(nameData); // Put name
+        messageData.putShort((short)passData.length); // Put pass length
+        messageData.put(passData); // Put pass
+
+        return messageData.array();
     }
 }
