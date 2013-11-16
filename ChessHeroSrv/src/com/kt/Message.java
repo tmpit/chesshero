e35ad67903e33d8d8967d354cb77101242748bb4
Todor Pitekov <esembidi@gmail.com>
2013-11-16 14:02:51 +0200

    now parsing a register and login message

--- ChessHeroSrv/src/com/kt/Message.java
+++ ChessHeroSrv/src/com/kt/Message.java
@@ -1,5 +1,8 @@
 package com.kt;
 
+import java.nio.BufferUnderflowException;
+import java.nio.ByteBuffer;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Toshko
@@ -7,19 +10,78 @@ package com.kt;
  * Time: 1:07 AM
  * To change this template use File | Settings | File Templates.
  */
+
 public class Message
 {
     public static final int HEADER_LENGTH = 2;
-    public static enum ActionCode
+
+    public static final short ACTION_REGISTER = 1;
+    public static final short ACTION_LOGIN = 2;
+    public static final short ACTION_MOVE = 3;
+
+    public static Message fromData(byte data[]) throws ChessHeroException
     {
-        REGISTER,
-        LOGIN,
-        MOVE
+        ByteBuffer buf = ByteBuffer.allocate(data.length);
+        buf.put(data);
+
+        // Read action number
+        short action = buf.getShort();
+
+        try
+        {
+            switch (action)
+            {
+                case ACTION_REGISTER:
+                case ACTION_LOGIN:
+                    Credentials credentials = readCredentials(buf);
+                    return new AuthMessage(action, credentials);
+
+                case ACTION_MOVE:
+
+                    break;
+
+                default:
+                    throw new ChessHeroException(ChessHeroException.INVALID_ACTION_ERROR);
+            }
+
+            return null;
+        }
+        catch (ChessHeroException e)
+        {
+            throw e;
+        }
     }
 
-    public static Message fromData(byte data[])
+    private static Credentials readCredentials(ByteBuffer buf) throws ChessHeroException
     {
-        return new Message();
+        try
+        {
+            short nameLen = buf.getShort();
+            if (nameLen < Credentials.MIN_NAME_LENGTH || nameLen > Credentials.MAX_NAME_LENGTH)
+            {
+                throw new ChessHeroException(ChessHeroException.INVALID_NAME_ERROR);
+            }
+
+            // Read name bytes
+            byte nameData[] = new byte[nameLen];
+            buf.get(nameData, 0, nameLen);
+
+            short passLen = buf.getShort();
+            if (passLen < Credentials.MIN_PASS_LENGTH || passLen > Credentials.MAX_PASS_LENGTH)
+            {
+                throw new ChessHeroException(ChessHeroException.INVALID_PASS_ERROR);
+            }
+
+            // Read pass bytes
+            byte passData[] = new byte[passLen];
+            buf.get(passData, 0, passLen);
+
+            return new Credentials(new String(nameData), new String(passData));
+        }
+        catch (BufferUnderflowException e)
+        {
+            throw new ChessHeroException();
+        }
     }
 
     public byte[] toData()
