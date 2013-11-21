2d0b60a30b9bd7fdc3a647ef4d2541082a27afd7
Todor Pitekov <esembidi@gmail.com>
2013-11-22 00:32:46 +0200

    added class AuthPair; Credentials class now has ivar AuthPair and generates hash and salt with java.security.SecureRandom

--- ChessHeroSrv/src/com/kt/Credentials.java
+++ ChessHeroSrv/src/com/kt/Credentials.java
@@ -2,6 +2,7 @@ package com.kt;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.Formatter;
 
 /**
@@ -19,22 +20,35 @@ public class Credentials
     public static final short MIN_PASS_LENGTH = 3;
     public static final short MAX_PASS_LENGTH = 50;
 
-    private String name;
-    private String pass;
+    private static SecureRandom csprng = new SecureRandom();
 
-    Credentials(String name, String pass) throws ChessHeroException
+    private String name = null;
+    private String pass = null;
+    private AuthPair authPair = null;
+
+    public static boolean isNameValid(String name)
     {
-        if (name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH)
-        {
-            throw new ChessHeroException(Result.INVALID_NAME);
-        }
-        if (pass.length() < MIN_PASS_LENGTH || pass.length() > MAX_PASS_LENGTH)
-        {
-            throw new ChessHeroException(Result.INVALID_PASS);
-        }
+        int length = name.length();
+        return (length >= MIN_NAME_LENGTH && length <= MAX_NAME_LENGTH);
+    }
 
+    public static boolean isPassValid(String pass)
+    {
+        int length = pass.length();
+        return (length >= MIN_PASS_LENGTH && length <= MAX_PASS_LENGTH);
+    }
+
+    Credentials(String name, String pass)
+    {
         this.name = name;
         this.pass = pass;
+    }
+
+    Credentials(String name, String pass, AuthPair authPair)
+    {
+        this.name = name;
+        this.pass = pass;
+        this.authPair = authPair;
     }
 
     public String getName()
@@ -47,32 +61,65 @@ public class Credentials
         return pass;
     }
 
-    public String getPassSHA1() throws ChessHeroException
+    public AuthPair getAuthPair() throws NoSuchAlgorithmException
     {
-        try
+        if (null == authPair)
         {
-            MessageDigest converter = MessageDigest.getInstance("SHA-1");
-            byte converted[] = converter.digest(pass.getBytes());
-
-            Formatter formatter = new Formatter();
-
-            for (byte b : converted)
-            {
-                formatter.format("%02x", b);
-            }
-
-            return formatter.toString();
+            int salt = csprng.nextInt(Integer.MAX_VALUE);
+            String salted = salt + pass;
+            String hash = hashOfString(salted);
+            authPair = new AuthPair(hash, salt);
         }
-        catch (NoSuchAlgorithmException e)
+
+        return authPair;
+    }
+
+    private static String hashOfString(String str) throws NoSuchAlgorithmException
+    {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte converted[] = digest.digest(str.getBytes());
+
+        Formatter formatter = new Formatter();
+
+        for (byte b : converted)
         {
-            SLog.write("Could not create password sha1: " + e);
-            throw new ChessHeroException(Result.INTERNAL_ERROR);
+            formatter.format("%02x", b);
         }
+
+        return formatter.toString();
     }
 
     @Override
     public String toString()
     {
-        return "<Credentials: name: " + name + ", pass: " + pass + ">";
+        return "<Credentials: name: " + name + ", pass: " + pass + ", authPair: " + authPair + ">";
+    }
+}
+
+class AuthPair
+{
+    private String hash = null;
+    private int salt;
+
+    AuthPair(String hash, int salt)
+    {
+        this.hash = hash;
+        this.salt = salt;
+    }
+
+    public String getHash()
+    {
+        return hash;
+    }
+
+    public int getSalt()
+    {
+        return salt;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "<AuthPair: hash: " + hash + ", salt: " + salt + ">";
     }
 }
