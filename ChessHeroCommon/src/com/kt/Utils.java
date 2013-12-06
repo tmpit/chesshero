132bf3a57a4a272aa914777000ef469fa0aef503
Todor Pitekov <esembidi@gmail.com>
2013-12-07 00:53:22 +0200

    added method for converting int to bytes

--- ChessHeroCommon/src/com/kt/Utils.java
+++ ChessHeroCommon/src/com/kt/Utils.java
@@ -14,6 +14,17 @@ public class Utils
         return (short)((bytes[index] << 8) | (bytes[index + 1] & 0xFF));
     }
 
+    public static byte[] bytesFromInt(int i)
+    {
+        byte data[] = new byte[4];
+        data[0] = (byte)((i >>> 24) & 0xFF);
+        data[1] = (byte)((i >>> 16) & 0xFF);
+        data[2] = (byte)((i >>> 8) & 0xFF);
+        data[3] = (byte)(i & 0xFF);
+
+        return data;
+    }
+
     public static byte[] bytesFromShort(short s)
     {
         byte data[] = new byte[2];
