b5c87ad9db1749b54e2b22688a7e51c7111285df
X0rrify <teodor.h.rusev@gmail.com>
2013-12-07 17:20:10 +0200

    added client handler and db connection

---
+++ ChessHeroChatSrv/src/com/chessherochatsrv/ChessHeroChatSrv.java
@@ -0,0 +1,25 @@
+package com.chessherochatsrv;
+
+import java.net.*;
+import java.util.*;
+
+public class ChessHeroChatSrv {
+	static final int PORT = 333;
+	static ServerSocket socket;
+	static Vector<ClientHandler> threadPool = new Vector<ClientHandler>();
+
+	public static void main(String[] args) {
+		try {
+			socket = new ServerSocket(PORT);
+			System.out.println("Server is started!");
+
+			while (true) {
+				Socket incoming = socket.accept();
+				System.out.println("New client connected!");
+				new ClientHandler(incoming, threadPool).start();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
