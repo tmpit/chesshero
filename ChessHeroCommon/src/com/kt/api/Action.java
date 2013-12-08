a6ec787b9cec23dd00dd03b7e2670ae771e68805
Todor Pitekov <esembidi@gmail.com>
2013-12-08 17:41:25 +0200

    added better api description

--- ChessHeroCommon/src/com/kt/api/Action.java
+++ ChessHeroCommon/src/com/kt/api/Action.java
@@ -5,23 +5,31 @@ package com.kt.api;
  */
 public class Action
 {
-    // About the notation:
+    // About the notation used:
     // On the left side of the arrow is the actual name of the parameter that should be used.
-    // On the right side of the arrow is the type of value the server will expect
+    // On the right side of the arrow is the type of value the server will expect.
+
+    // Each request to the server should always have at least one parameter 'action' => [INT].
+    // Each response from the server should have at least one parameter 'result' => [INT].
+    // Each message from the server that is not a response to a request should have parameter 'push' => [BOOL] which is always true.
+    // The following are all action codes recognized by the server, their additional parameters required for each action,
+    // and all additional parameters returned if the request has succeeded:
 
     // Login
-    // Parameters: username => [STR], password => [STR]
+    // Parameters: 'username' => [STR], 'password' => [STR]
+    // Returns: 'username' => [STR], 'userid' => [INT]
     public static final int LOGIN = 1;
 
     // Register
-    // Parameters: username => [STR], password => [STR]
+    // Parameters: 'username' => [STR], 'password' => [STR]
+    // Returns: 'username' => [STR], 'userid' => [INT]
     public static final int REGISTER = 2;
 
     // Create a game
-    // Parameters: gamename => [STR]
+    // Parameters: 'gamename' => [STR]
     public static final int CREATE_GAME = 3;
 
     // Cancel a pending game
-    // Parameters: gameid => [INT]
+    // Parameters: 'gameid' => [INT]
     public static final int CANCEL_GAME = 4;
 }
