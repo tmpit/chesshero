7783213e5d7062838e7e5cfc94f5247f0b9af380
Todor Pitekov <esembidi@gmail.com>
2013-12-09 00:27:59 +0200

    game object can now join players; added various getters; added method for generating chat tokens

--- ChessHeroSrv/src/com/kt/Game.java
+++ ChessHeroSrv/src/com/kt/Game.java
@@ -1,7 +1,11 @@
 package com.kt;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.HashMap;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,10 +18,11 @@ public class Game
 {
     public static short STATE_INIT      = 0; // Just initialized, not yet in the list
     public static short STATE_PENDING   = 1; // Inside the list with created games
-    public static short STATE_STARTED   = 2; // The game is being played
-    public static short STATE_FINISHED  = 3; // The game has finished
+    public static short STATE_STARTING  = 2; // The game is about to be started
+    public static short STATE_STARTED   = 3; // The game is being played
+    public static short STATE_FINISHED  = 4; // The game has finished
 
-    private static ArrayList<Game> games = new ArrayList<Game>();
+    private static HashMap<Integer, Game> games = new HashMap<Integer, Game>();
     private static Database database = new Database();
 
     private int gameID;
@@ -32,9 +37,45 @@ public class Game
 
     private short state = STATE_INIT;
 
-    public static synchronized void addGame(Game game)
+    public static void addGame(Game game)
     {
-        games.add(game);
+        synchronized (games)
+        {
+            games.put(game.gameID, game);
+        }
+    }
+
+    public static void removeGame(int gameID)
+    {
+        synchronized (games)
+        {
+            games.remove(gameID);
+        }
+    }
+
+    public static Game getGame(int gameID)
+    {
+        synchronized (games)
+        {
+            return games.get(gameID);
+        }
+    }
+
+    public static String generateChatToken(int gameID, int userID, String gameName) throws NoSuchAlgorithmException
+    {
+        String cat1 = gameName + gameID + userID;
+
+        MessageDigest digest = MessageDigest.getInstance("SHA-1");
+        byte tokenData[] = digest.digest(cat1.getBytes());
+
+        Formatter formatter = new Formatter();
+
+        for (byte b : tokenData)
+        {
+            formatter.format("%02x", b);
+        }
+
+        return formatter.toString();
     }
 
     public Game(int gameID, String name, ClientConnection initiator)
@@ -45,6 +86,22 @@ public class Game
         this.user1ID = initiator.getUserID();
     }
 
+    public void join(ClientConnection secondPlayer)
+    {
+        client2 = secondPlayer;
+        user2ID = secondPlayer.getUserID();
+    }
+
+    public int getGameID()
+    {
+        return gameID;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
     public short getState()
     {
         return state;
@@ -53,5 +110,25 @@ public class Game
     public void setState(short state)
     {
         this.state = state;
+    }
+
+    public ClientConnection getOtherClient(ClientConnection not)
+    {
+        if (client1 == not)
+        {
+            return client2;
+        }
+
+        return client1;
+    }
+
+    public int getOtherUserID(int not)
+    {
+        if (user1ID == not)
+        {
+            return user2ID;
+        }
+
+        return user1ID;
     }
 }
