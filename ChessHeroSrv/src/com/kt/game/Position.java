d770384318b77e3cc6075f1b2335844ec68e4e50
Todor Pitekov <esembidi@gmail.com>
2013-12-20 18:35:49 +0200

    added position class

---
+++ ChessHeroSrv/src/com/kt/game/Position.java
@@ -0,0 +1,26 @@
+package com.kt.game;
+
+/**
+ * Created by Toshko on 12/20/13.
+ */
+public class Position
+{
+	protected int x;
+	protected int y;
+
+	public Position(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX()
+	{
+		return x;
+	}
+
+	public int getY()
+	{
+		return y;
+	}
+}
