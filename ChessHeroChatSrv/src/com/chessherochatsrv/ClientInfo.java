3c0135d2ba6e31f4b170c23d1bd84bbc2ed17926
Todor Pitekov <esembidi@gmail.com>
2014-05-21 10:13:47 +0300

    removed the chat that was not

--- ChessHeroChatSrv/src/com/chessherochatsrv/ClientInfo.java
+++
@@ -1,9 +0,0 @@
-package com.chessherochatsrv;
-
-import java.net.Socket;
-
-public class ClientInfo {
-	public Socket mSocket = null;
-	public ClientListener mClientListener = null;
-	public ClientSender mClientSender = null;
-}
