f986e3b30152707b64c7e50ddb1608e60ce686d7
Todor Pitekov <esembidi@gmail.com>
2014-11-26 22:20:50 +0200

    implemented base message handling structure; defined proxy interface

--- ChessHeroAndroidClient/app/src/main/java/com/chesshero/service/ServerCommunicationService.java
+++ ChessHeroAndroidClient/app/src/main/java/com/chesshero/service/ServerCommunicationService.java
@@ -2,8 +2,12 @@ package com.chesshero.service;
 
 import android.app.Service;
 import android.content.Intent;
-import android.os.Binder;
-import android.os.IBinder;
+import android.os.*;
+import android.os.Process;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Toshko on 11/24/14.
@@ -16,10 +20,73 @@ public class ServerCommunicationService extends Service
 	private static final int READ_TIMEOUT = 15 * 1000; // In milliseconds
 	private static final int WRITE_TIMEOUT = 15 * 1000; // In milliseconds
 
+	private static final int MSG_ACTION_CONNECT = 1;
+	private static final int MSG_ACTION_DISCONNECT = 2;
+	private static final int MSG_ACTION_REQUEST = 3;
+
+	private MessageHandler messageHandler;
+
+	private ArrayList<ServiceEventListener> eventListeners = new ArrayList<ServiceEventListener>();
+
+	private void connect()
+	{
+
+	}
+
+	private void disconnect()
+	{
+
+	}
+
+	private void sendRequest(ServiceRequest request)
+	{
+
+	}
+
+	private synchronized void addEventListener(ServiceEventListener listener)
+	{
+		eventListeners.add(listener);
+	}
+
+	private synchronized void removeEventListener(ServiceEventListener listener)
+	{
+		eventListeners.remove(listener);
+	}
+
+	private void onHandleMessage(Message msg)
+	{
+		switch (msg.what)
+		{
+			case MSG_ACTION_CONNECT:
+				connect();
+				break;
+
+			case MSG_ACTION_DISCONNECT:
+				disconnect();
+				break;
+
+			case MSG_ACTION_REQUEST:
+				if (msg.obj != null && msg.obj instanceof ServiceRequest) {
+					sendRequest((ServiceRequest) msg.obj);
+				} else {
+					Log.w("Invalid or missing ServiceRequest object", "");
+				}
+				break;
+
+			default:
+				Log.w("Invalid message action", "");
+		}
+	}
+
 	@Override
 	public void onCreate()
 	{
 		super.onCreate();
+
+		HandlerThread thread = new HandlerThread("com.chesshero.scs.handler", Process.THREAD_PRIORITY_BACKGROUND);
+		thread.start();
+
+		messageHandler = new MessageHandler(thread.getLooper());
 	}
 
 	@Override
@@ -37,11 +104,52 @@ public class ServerCommunicationService extends Service
 	@Override
 	public IBinder onBind(Intent intent)
 	{
-		return new ProxyBinder();
+		return new ServiceProxy();
 	}
 
-	public class ProxyBinder extends Binder
+	public class ServiceProxy extends Binder
 	{
+		public void addEventListener(ServiceEventListener listener)
+		{
+			ServerCommunicationService.this.addEventListener(listener);
+		}
 
+		public void removeEventListener(ServiceEventListener listener)
+		{
+			ServerCommunicationService.this.removeEventListener(listener);
+		}
+
+		public void connect()
+		{
+			Message msg = messageHandler.obtainMessage(MSG_ACTION_CONNECT);
+			messageHandler.sendMessage(msg);
+		}
+
+		public void disconnect()
+		{
+			Message msg = messageHandler.obtainMessage(MSG_ACTION_DISCONNECT);
+			messageHandler.sendMessage(msg);
+		}
+
+		public void sendRequest(ServiceRequest request)
+		{
+			Message msg = messageHandler.obtainMessage(MSG_ACTION_REQUEST, request);
+			messageHandler.sendMessage(msg);
+		}
+	}
+
+	private class MessageHandler extends Handler
+	{
+		MessageHandler(Looper looper)
+		{
+			super(looper);
+		}
+
+		@Override
+		public void handleMessage(Message msg)
+		{
+			super.handleMessage(msg);
+			ServerCommunicationService.this.onHandleMessage(msg);
+		}
 	}
 }
