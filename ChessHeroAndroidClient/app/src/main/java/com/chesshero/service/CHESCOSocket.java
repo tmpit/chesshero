38a7d7255d2295e675b31a30456bd7e57dc15ac2
Todor Pitekov <esembidi@gmail.com>
2014-12-03 00:17:33 +0200

    greatly thinned out the CHESCOSocket wrapper

--- ChessHeroAndroidClient/app/src/main/java/com/chesshero/service/CHESCOSocket.java
+++ ChessHeroAndroidClient/app/src/main/java/com/chesshero/service/CHESCOSocket.java
@@ -15,105 +15,25 @@ import com.kt.utils.SLog;
 public class CHESCOSocket
 {
 	private Socket sock = null;
-	private boolean connected = false;
 	private CHESCOWriter writer;
 	private CHESCOReader reader;
 
-	private void log(String text)
+	CHESCOSocket(Socket socket) throws IOException
 	{
-		SLog.write("[CHESCOSocket] : " + text);
-	}
-
-	private void cleanUp()
-	{
-		sock = null;
-		writer = null;
-		reader = null;
-	}
-
-	public boolean isConnected()
-	{
-		return connected;
-	}
-
-	public void connect(String address, int port, int connectionTimeout) throws IOException
-	{
-		if (connected)
-		{
-			log("attempting to connect while connected. It's OK, I've got your back, sister.");
-			return;
-		}
-
-		sock = new Socket();
-		sock.connect(new InetSocketAddress(address, port), connectionTimeout);
-		sock.setKeepAlive(true);
-
+		sock = socket;
 		writer = new CHESCOWriter(sock.getOutputStream());
 		reader = new CHESCOReader(sock.getInputStream());
-
-		connected = true;
-	}
-
-	public void disconnect()
-	{
-		if (!connected)
-		{
-			log("attempting to disconnect while disconnected. It's OK, I've got your back, sister.");
-			return;
-		}
-
-		try
-		{
-			sock.close();
-		}
-		catch (IOException e)
-		{
-			log("exception on disconnect: " + e);
-		}
-		finally
-		{
-			connected = false;
-			cleanUp();
-		}
 	}
 
 	public void write(Map map, int timeout) throws IOException
 	{
-		if (!connected)
-		{
-			log("attempting to write to a disconnected socket. Epic fail...");
-		}
-
-		try
-		{
-			sock.setSoTimeout(timeout);
-			writer.write(map);
-		}
-		catch (IOException e)
-		{
-			connected = false;
-			cleanUp();
-			throw e;
-		}
+		sock.setSoTimeout(timeout);
+		writer.write(map);
 	}
 
 	public Map read(int timeout) throws IOException
 	{
-		if (!connected)
-		{
-			log("attempting to read from a disconnected socket. Epic fail...");
-		}
-
-		try
-		{
-			sock.setSoTimeout(timeout);
-			return (Map)reader.read();
-		}
-		catch (IOException e)
-		{
-			connected = false;
-			cleanUp();
-			throw e;
-		}
+		sock.setSoTimeout(timeout);
+		return (Map)reader.read();
 	}
 }
