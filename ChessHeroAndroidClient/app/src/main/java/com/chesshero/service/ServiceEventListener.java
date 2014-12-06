6132a3d55cfa4138b16fc85b17e1f9bdd34459c0
Todor Pitekov <esembidi@gmail.com>
2014-12-06 23:55:43 +0200

    added comments

--- ChessHeroAndroidClient/app/src/main/java/com/chesshero/service/ServiceEventListener.java
+++ ChessHeroAndroidClient/app/src/main/java/com/chesshero/service/ServiceEventListener.java
@@ -7,9 +7,20 @@ import java.util.HashMap;
  */
 public interface ServiceEventListener
 {
+	// Invoked when the service successfully establishes a connection to the server
 	void serviceDidConnect();
+
+	// Invoked when the service fails to establish a connection to the server
 	void serviceDidFailToConnect();
+
+	// Invoked when the service closes the connection to the server wither due of a connection failure or due to a disconnect() call on the ServiceProxy
 	void serviceDidDisconnect();
+
+	// Invoked when the service executes a request
+	// The 'request' object will be the same one passed to the sendRequest() method
+	// The 'response' object will be null if the request timed out or if there has been a connection failure
 	void serviceDidCompleteRequest(ServiceRequest request, HashMap<String, Object> response);
+
+	// Invoked when the service receives a push message from the server
 	void serviceDidReceivePushMessage(HashMap<String, Object> message);
 }
