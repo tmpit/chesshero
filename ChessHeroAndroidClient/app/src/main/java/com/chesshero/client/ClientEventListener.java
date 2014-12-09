cc454a177bb8980d758046e595255af751513269
Todor Pitekov <esembidi@gmail.com>
2014-12-09 23:19:21 +0200

    removed client event listener interface - it will be replaced by the event center

--- ChessHeroAndroidClient/app/src/main/java/com/chesshero/client/ClientEventListener.java
+++
@@ -1,9 +0,0 @@
-package com.chesshero.client;
-
-/**
- * Created by Toshko on 12/8/14.
- */
-public interface ClientEventListener
-{
-	public void clientDidCompleteLogin(Integer result, User user);
-}
