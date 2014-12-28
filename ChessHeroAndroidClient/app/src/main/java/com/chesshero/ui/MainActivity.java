7407f866d48166c932be99831c5425929e78c586
Vasil Marinkov <dp.lmnt@gmail.com>
2014-12-28 14:08:03 +0200

    login and register now working

--- ChessHeroAndroidClient/app/src/main/java/com/chesshero/ui/MainActivity.java
+++ ChessHeroAndroidClient/app/src/main/java/com/chesshero/ui/MainActivity.java
@@ -4,20 +4,29 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.chesshero.R;
+import com.chesshero.client.ChessHeroApplication;
+import com.chesshero.client.Client;
+import com.chesshero.event.EventCenter;
+import com.chesshero.event.EventCenterObserver;
+import com.kt.api.Result;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements EventCenterObserver {
 
     private Intent pageToOpen;
+
+    private Client client;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // setting default screen to login.xml
         setContentView(R.layout.login);
+
+        client = ((ChessHeroApplication) getApplication()).getClient();
+        EventCenter.getSingleton().addObserver(this, Client.Event.LOGIN_RESULT);
     }
 
     public void openRegisterPage(View view) {
@@ -25,9 +34,25 @@ public class MainActivity extends Activity {
         startActivity(pageToOpen);
     }
 
-    public void openPlayChessPage(View view) {
-        pageToOpen = new Intent(this, PlayChessActivity.class);
-        startActivity(pageToOpen);
+    //todo add field constrains/validations
+    public void login(View view) {
+        String username = ((EditText) findViewById(R.id.login_username)).getText().toString();
+        String password = ((EditText) findViewById(R.id.login_password)).getText().toString();
+
+        client.login(username, password);
+    }
+
+    @Override
+    public void eventCenterDidPostEvent(String eventName, Object userData) {
+        if (eventName == Client.Event.LOGIN_RESULT) {
+
+            //todo handle other result cases, open lobby
+
+            if (userData != null && (Integer) userData == Result.OK) {
+                pageToOpen = new Intent(this, PlayChessActivity.class);
+                startActivity(pageToOpen);
+            }
+        }
     }
 }
 
--- ChessHeroAndroidClient/app/src/main/java/com/chesshero/ui/RegisterActivity.java
+++ ChessHeroAndroidClient/app/src/main/java/com/chesshero/ui/RegisterActivity.java
@@ -8,24 +8,60 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.chesshero.R;
+import com.chesshero.client.ChessHeroApplication;
+import com.chesshero.client.Client;
+import com.chesshero.event.EventCenter;
+import com.chesshero.event.EventCenterObserver;
+import com.kt.api.Result;
 
 
-public class RegisterActivity extends Activity {
+public class RegisterActivity extends Activity implements EventCenterObserver {
 
     private Intent pageToOpen;
+
+    private Client client;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Set View to register.xml
         setContentView(R.layout.register);
+
+        client = ((ChessHeroApplication) getApplication()).getClient();
+        EventCenter.getSingleton().addObserver(this, Client.Event.REGISTER_RESULT);
     }
 
     public void openLoginPage(View view) {
         pageToOpen = new Intent(this, MainActivity.class);
         startActivity(pageToOpen);
+    }
+
+    //todo add more password/username constraints/validations
+    public void register(View view) {
+        String username = ((EditText) findViewById(R.id.reg_username)).getText().toString();
+        String password = ((EditText) findViewById(R.id.reg_password)).getText().toString();
+        String password2 = ((EditText) findViewById(R.id.reg_password2)).getText().toString();
+
+        if (!password.equals(password2)) {
+            Toast.makeText(RegisterActivity.this, "Password does not match", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        client.register(username, password);
+    }
+
+    @Override
+    public void eventCenterDidPostEvent(String eventName, Object userData) {
+        if (eventName == Client.Event.REGISTER_RESULT) {
+            Toast.makeText(RegisterActivity.this, "did complete register with result code " + userData, Toast.LENGTH_SHORT).show();
+
+            if (userData != null && (Integer) userData == Result.OK) {
+                //todo automatic login to lobby / or return to login page?
+                Toast.makeText(RegisterActivity.this, client.getPlayer().toString(), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
