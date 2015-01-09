ce0551b492c017c1f8a1b0b5ebbc8b1ce3d6f58c
Vasil Marinkov <dp.lmnt@gmail.com>
2015-01-09 21:54:09 +0200

    use Move instead of code String

--- ChessHeroAndroidClient/app/src/main/java/com/chesshero/ui/PlayChessActivity.java
+++ ChessHeroAndroidClient/app/src/main/java/com/chesshero/ui/PlayChessActivity.java
@@ -145,31 +145,31 @@ public class PlayChessActivity extends Activity implements EventCenterObserver {
         List<Move> moves = client.getGame().getExecutedMoves();
 
         if (eventName == Client.Event.MOVE_RESULT) {
-            drawMove(moves.get(moves.size() - 1).code);
+            drawMove(moves.get(moves.size() - 1));
             isMyTurn = false;
             //vij drugite raoti tuk w game obekta
             //  Log.i("", currentLastMove.executor.getName());
         }
         if (eventName == Client.Event.MOVE_PUSH) {
-            drawMove(moves.get(moves.size() - 1).code);
+            drawMove(moves.get(moves.size() - 1));
             isMyTurn = true;
         }
     }
 
-    private void drawMove(String code) {
+    private void drawMove(Move move) {
         int startCol, startRow, endCol, endRow;
         // flipped - true
         if (isFlipped) {
-            startCol = 104 - code.charAt(0);
-            startRow = Integer.parseInt(code.charAt(1) + "") - 1;
-            endCol = 104 - code.charAt(2);
-            endRow = Integer.parseInt(code.charAt(3) + "") - 1;
+            startCol = 104 - move.code.charAt(0);
+            startRow = Integer.parseInt(move.code.charAt(1) + "") - 1;
+            endCol = 104 - move.code.charAt(2);
+            endRow = Integer.parseInt(move.code.charAt(3) + "") - 1;
         } else {
             // flipped - false
-            startCol = code.charAt(0) - 97;
-            startRow = 8 - Integer.parseInt(code.charAt(1) + "");
-            endCol = code.charAt(2) - 97;
-            endRow = 8 - Integer.parseInt(code.charAt(3) + "");
+            startCol = move.code.charAt(0) - 97;
+            startRow = 8 - Integer.parseInt(move.code.charAt(1) + "");
+            endCol = move.code.charAt(2) - 97;
+            endRow = 8 - Integer.parseInt(move.code.charAt(3) + "");
         }
 
         Tile previousTile = adapter.getTileAt(startRow, startCol);
