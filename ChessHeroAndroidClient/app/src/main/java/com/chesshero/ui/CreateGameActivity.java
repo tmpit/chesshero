3fb529e1c3e78bd6bbffca74a39df9e1dd429f60
Vasil Marinkov <dp.lmnt@gmail.com>
2015-01-31 17:09:04 +0200

    Move Create Game Activity to the right place.

--- ChessHeroAndroidClient/app/src/main/java/com/chesshero/ui/CreateGameActivity.java
+++ ChessHeroAndroidClient/app/src/main/java/com/chesshero/ui/CreateGameActivity.java
@@ -1,47 +1,33 @@
-package com.chesshero.ui.chessboard;
+package com.chesshero.ui;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TableLayout;
 
 import com.chesshero.R;
 import com.chesshero.client.Client;
 import com.chesshero.event.EventCenter;
 import com.chesshero.event.EventCenterObserver;
-import com.chesshero.ui.LobbyActiviy;
-import com.chesshero.ui.MainActivity;
-import com.kt.api.Result;
 
 /**
  * Created by Lyubomira on 1/26/2015.
  */
 public class CreateGameActivity extends Activity implements EventCenterObserver {
 
-
     public static Client client;
-    private TableLayout table;
     private Intent pageToOpen;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_game_page);
 
-
         EventCenter.getSingleton().addObserver(this, Client.Event.CREATE_GAME_RESULT);
-
 
     }
 
-
-
-
-
-    public void backToLobby(View view){
-
+    public void backToLobby(View view) {
         pageToOpen = new Intent(this, LobbyActiviy.class);
         startActivity(pageToOpen);
     }
